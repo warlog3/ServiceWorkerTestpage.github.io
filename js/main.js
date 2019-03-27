6438341e54d05e9346aee206ed5b59a279e4c4ed
Daniel Jancin <D.Jancin@gmx.de>
2019-03-27 15:29:50 +0100

    push

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
+'use strict';
+
 let deferredPrompt;
 let status = document.getElementById("status");
 status.innerHTML = "main.js called";
+
+
+const applicationServerPublicKey = 'BOLVnljkkcqNKfi9myheLDtUlcbpmPkWIZkYtuHS054KCJHd_fWBPonOH832JqYFJY9AjzBzkJQwxUVs7ep1hwY';
+const pushButton = document.getElementById('subscribe');
+let isSubscribed = false;
+let swRegistration = null;
 
 caches.keys().then(function(names) {
   for (let name of names)
@@ -16,21 +24,84 @@ window.addEventListener('beforeinstallprompt', (e) => {
   deferredPrompt = e;
 
   btnAdd.style.display = "block";
-  // Show the prompt
-  /*deferredPrompt.prompt();
-
-  deferredPrompt.userChoice
-    .then( (choiceResult) => {
-      if(choiceResult.outcome === 'accepted'){
-
-      }
-      else{
-        console.log("User dismissed the add-to-homescreen-prompt!");
-      }
-      deferredPrompt = null;
-    });
-    */
 })
+
+function urlB64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/');
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
+function subscribeUser() {
+  const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+  swRegistration.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: applicationServerKey
+  })
+  .then(function(subscription) {
+    console.log('User is subscribed.');
+
+    updateSubscriptionOnServer(subscription);
+
+    isSubscribed = true;
+
+    updateBtn();
+  })
+  .catch(function(err) {
+    console.log('Failed to subscribe the user: ', err);
+    updateBtn();
+  });
+}
+
+function updateSubscriptionOnServer(subscription) {
+  // TODO: Send subscription to application server
+
+  const subscriptionJson = document.querySelector('.js-subscription-json');
+  const subscriptionDetails =
+    document.querySelector('.js-subscription-details');
+
+  if (subscription) {
+    subscriptionJson.textContent = JSON.stringify(subscription);
+    subscriptionDetails.classList.remove('is-invisible');
+  } else {
+    subscriptionDetails.classList.add('is-invisible');
+  }
+}
+
+function initializeUI() {
+  pushButton.addEventListener('click', function() {
+    pushButton.disabled = true;
+    if (isSubscribed) {
+      // TODO: Unsubscribe user
+    } else {
+      subscribeUser();
+    }
+  });
+
+
+  // Set the initial subscription value
+  swRegistration.pushManager.getSubscription()
+  .then(function(subscription) {
+    isSubscribed = !(subscription === null);
+
+    if (isSubscribed) {
+      console.log('User IS subscribed.');
+    } else {
+      console.log('User is NOT subscribed.');
+    }
+
+    updateBtn();
+  });
+}
 
 btnAdd.addEventListener('click', (e) => {
   // hide our user interface that shows our A2HS button
@@ -49,6 +120,21 @@ btnAdd.addEventListener('click', (e) => {
     });
 });
 
+function updateBtn() {
+if (Notification.permission === 'denied') {
+    pushButton.textContent = 'Push Messaging Blocked.';
+    pushButton.disabled = true;
+    updateSubscriptionOnServer(null);
+    return;
+  }
+
+  if (isSubscribed) {
+    console.log("Push ausschalten");
+  } else {
+    console.log("Push einschalten");
+  }
+}
+
 //Make sure SW are supported
 if('serviceWorker' in navigator){
   console.log('Service Worker supported!');
@@ -59,4 +145,32 @@ if('serviceWorker' in navigator){
       .then(reg => console.log("Service Worker registered"))
       .catch(err => console.log(`Error + ${err}`));
   });
+
+  //Add service worker responsible for push-messages
+  /*
+  if('PushManager' in window){
+    navigator.serviceWorker.register('sw_push.js')
+      .then(function(swReg) {
+        console.log('Service Worker is registered', swReg);
+
+        swRegistration = swReg;
+        initializeUI();
+      })
+
+    console.log('Push notifications supported');
+
+    navigator.serviceWorker.register('sw_push.js')
+    .then(function(swReg) {
+      console.log('Service Worker is registered', swReg);
+
+      swRegistration = swReg;
+    })
+    .catch(function(error) {
+      console.error('Service Worker Error', error);
+    });
+  } 
+  else {
+    console.warn('Push messaging NOT supported');
+    pushButton.textContent = 'Push Not Supported';
+  }*/
 }
--- sw_cached_pages.js
+++ sw_cached_pages.js
@@ -48,3 +48,17 @@ self.addEventListener('fetch', event => {
     fetch(event.request).catch( () => {caches.match(event.request)})
   );
 })
+
+self.addEventListener('push', function(event) {
+  console.log('[Service Worker] Push Received.');
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+  const title = 'Push Codelab';
+  const options = {
+    body: 'Yay it works.',
+    icon: 'images/icon.png',
+    badge: 'images/badge.png'
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
