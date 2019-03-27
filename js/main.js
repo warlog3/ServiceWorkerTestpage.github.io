7b2691a32a8aec836d077b9a077e8d4799e751ef
Daniel Jancin <D.Jancin@gmx.de>
2019-03-27 09:53:23 +0100

    prompt

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  let headerColor = document.getElementById("headerColor");
+  headerColor.style.color = 'white';
+  /*
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+
+  // Update UI notify the user they can add to home screen
+  btnAdd.style.display = 'block';
+  */
+})
+
 //Make sure SW are supported
 if('serviceWorker' in navigator){
   console.log('Service Worker supported!');
