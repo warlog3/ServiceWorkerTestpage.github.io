28a4744dd571c81aa10d062346c4777bcdc89357
Daniel Jancin <D.Jancin@gmx.de>
2019-03-27 11:12:42 +0100

    installprompt android

--- js/main.js
+++ js/main.js
@@ -32,6 +32,23 @@ window.addEventListener('beforeinstallprompt', (e) => {
     */
 })
 
+btnAdd.addEventListener('click', (e) => {
+  // hide our user interface that shows our A2HS button
+  btnAdd.style.display = 'none';
+  // Show the prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice
+    .then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+});
+
 //Make sure SW are supported
 if('serviceWorker' in navigator){
   console.log('Service Worker supported!');
